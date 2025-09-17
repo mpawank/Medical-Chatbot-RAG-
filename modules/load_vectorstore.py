6757e3bc30b3ea2a39d453c770c5560bab6e2305
M PAWAN KUMAR <146627172+mpawank@users.noreply.github.com>
2025-09-18 00:29:29 +0530

    phase one complated

---
+++ modules/load_vectorstore.py
@@ -0,0 +1,9 @@
+import os
+import time
+from pathlib import Path
+from dotenv import load_dotenv
+from tqdm.auto import tqdm
+from pinecone import Pinecone, ServerlessSpec
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
